47b3d3577da96900cf8b3d08e03cba9961444de8
Zhang Jionghuan <zhangjionghuan@tp-link.com.cn>
2020-01-03 17:55:29 +0800

    Re-copnstruct project data mode, get data from API request

--- src/pages/Project-List.vue
+++ src/pages/Project-List.vue
@@ -18,56 +18,48 @@
 
 <script>
 import YImageCard from '../components/yuko-md-components/YImageCard'
+import axios from 'axios'
 
 export default {
   data() {
     return {
-      projectList: [
-        {
-          title: '64 hexagram in I Ching',
-          abstract: 'A JavaScript implement of 64 hexagrams in I Ching',
-          description: `This is a JavaScript implement of 64 hexagrams in I Ching. The I Ching book consists of 64 hexagrams. 
-                        A hexagram in this context is a figure composed of six stacked horizontal lines (爻 yáo), 
-                        where each line is either Yang (an unbroken, or solid line), or Yin (broken, an open line with a gap in the center). 
-                        The hexagram lines are traditionally counted from the bottom up, so the lowest line is considered line one while the top line is line six. 
-                        Hexagrams are formed by combining the original eight trigrams in different combinations. Each hexagram is accompanied with a description, 
-                        often cryptic, akin to parables. Each line in every hexagram is also given a similar description.
-                        The Chinese word for a hexagram is 卦 "guà", although that also means trigram.`,
-          link: '#/project/hexagram',
-          imgLink: require('../images/project/hexagram.png')
-        },
-        {
-          title: 'Yuko Material Design Components',
-          abstract: 'A web components library follow Material Design specification',
-          description: `This is a JavaScript/CSS implement of web component library follow Material Design specification. 
-                        Material Design is a visual language that synthesizes the classic principles of good design with the innovation of technology and science. 
-                        Material Design's goals: 1. Create a visual language that synthesizes the classic principles of good design with the innovation and possibility of technology and science. 
-                        2. Develop a single underlying system that unifies the user experience across platforms, devices, and input methods. 
-                        3. Expand Material’s visual language and provide a flexible foundation for innovation and brand expression.`,
-          link: '#/project/md-components',
-          imgLink: require('../images/project/md-components.png')
-        },
-        {
-          title: 'Simple Canvas Snow Effect',
-          abstract: 'A JavaScript implement of snow effect using canvas',
-          description: `A simple implement of snow effect, which take effect on background image and cursor.`,
-          link: '#/project/snow-effect',
-          imgLink: require('../images/project/canvas-snow-effect.png')
-        },
-        {
-          title: 'Material Design Navigation Drawer',
-          abstract: 'A JavaScript implement of Navigation Drawer on mobile device',
-          description: `This project is a JavaScript implement of material design Navigation Drawer on mobile device. 
-                        As one of my early projects, it is coded with raw javascript without other third-party libraries. 
-                        For more information, see https://github.com/RyougiChan/Yuko.js/`,
-          link: '#/project/navigation-drawer',
-          imgLink: require('../images/project/navigation-drawer.png')
-        }
-      ]
+      projectList: []
     }
   },
   components: {
     YImageCard
+  },
+  mounted () {
+      window.console.log(this);
+      window.console.log(this.projectList);
+
+    axios.post(process.env.VUE_APP_APIURL + '/project/list', {
+      page: 1,
+      limit: 8,
+      orderBy: {
+        property: 'modifiedTime'
+      }
+    })
+    .then((response) => {
+      window.console.log(response);
+      let resData = response.data;
+      this.projectList = [];
+      window.console.log(this);
+      window.console.log(this.projectList);
+      Array.from(resData.result.content).forEach(d => {
+        this.projectList.push({
+          title: d.title,
+          abstract: d.abs,
+          description: d.content,
+          link: '#/project/' + d.id,
+          imgLink: process.env.VUE_APP_IMGURL + d.coverUrl
+        })
+      });
+      window.console.log(this.projectList.length);
+    })
+    .catch((error) => {
+      window.console.log(error);
+    });
   },
 }
 </script>
